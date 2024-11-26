b2b6e3844c3fece0e3bf2e77a035808080537fd8
LucaPalminteri <lucapalminteri02@gmail.com>
2024-11-26 17:04:54 -0300

    feat: add more metadata to the site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,26 +16,42 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Secret Santa",
-  description: "A simple Secret Santa app built with Next.js",
+  description: "Organize a Secret Santa gift exchange effortlessly with this app.",
+  keywords: ["Secret Santa", "Gift Exchange", "Holiday App", "Next.js Project", "Organize Gift Swaps"],
+  authors: [
+    {
+      name: "Luca Palminteri",
+      url: "https://github.com/LucaPalminteri",
+    },
+  ],
+  themeColor: "#FF0000",
   openGraph: {
     title: "Secret Santa",
-    description: "A simple Secret Santa app built with Next.js",
+    description: "Organize a Secret Santa gift exchange effortlessly with this app.",
     url: "https://secret-santa-blue-six.vercel.app",
     type: "website",
+    siteName: "Secret Santa App",
+    locale: "en_US",
     images: [
       {
         url: "https://secret-santa-blue-six.vercel.app/santa.jpg",
         width: 1200,
         height: 630,
         alt: "Secret Santa app preview image",
+        type: "image/jpeg",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Secret Santa",
-    description: "A simple Secret Santa app built with Next.js",
+    description: "Organize a Secret Santa gift exchange effortlessly with this app.",
     images: ["https://secret-santa-blue-six.vercel.app/santa.jpg"],
+  },
+  viewport: "width=device-width, initial-scale=1.0",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/favicon.ico",
   },
 };
 
@@ -46,7 +62,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}<Analytics /></body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
